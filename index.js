b66529140dc04f6f3fba14e01be44bfa3333f054
Benarba Tewfik <tewfikhotspur@gmail.com>
2023-08-26 10:03:36 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  let number = Math.floor(word.length / 2);
+  for (let i = 0; i < number; i++) {
+    if (word.at(i) !== word.at(word.length - i - 1)) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /* 
   Add your pseudocode here
+  functionisPalindrom(word):
+  number is floor of word length divided by 2
+   for i from 0 to number - 1 and keep adding 1 to i
+     if the character at index i not equls the character at index word length -i -1 
+        then return false
+  return true
+
+create a condition inside the loop to check if the first charcter not equls the last characters return false and contunue the iteration comparing each word with it match
+from the other side 
+
+outside the loop if the condition is not met in all the word return true; 
 */
 
 /*
